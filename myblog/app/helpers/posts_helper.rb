f55aedbb82a6806e4799db0d8bccb92d03f13d19
Trent Casey <trcasey@gmail.com>
2011-08-04 14:28:19 -0700

    basic structure finished.

---
+++ myblog/app/helpers/posts_helper.rb
@@ -0,0 +1,5 @@
+module PostsHelper
+
+	@posts = Post.all
+
+end
---
+++ myblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = '6f7802fdf141fdf7ca24c2a67613bb9ef1acf72f75f2a924417923802246a1aa6112c62447166b97db6420add446ee3e54ae280fb79b027e0b875ee226c9bdb0'
